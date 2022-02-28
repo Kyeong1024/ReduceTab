3c2cea6268ec8dc8bd242575a811bf2a74c069b6
Kyeong1024 <aofldjxm@gmail.com>
2022-02-28 19:48:46 +0900

    fix: gitignore setting

--- src/options/options.js
+++ src/options/options.js
@@ -1,3 +1,11 @@
 import "./style.css";
 
-console.log("option page");
+const $countOption = document.getElementById("count-number");
+
+$countOption.addEventListener("change", (event) => {
+  const number = event.target.value;
+
+  if (number < 5 || number > 25) {
+    $countOption.value = 5;
+  }
+});
