e5129951362d7ab4bd648baeca88725fc270b18e
Kyeong1024 <aofldjxm@gmail.com>
2022-03-01 20:24:54 +0900

    feat: connect tabCount and sorting  info  option and popup

--- src/options/options.js
+++ src/options/options.js
@@ -1,11 +1,29 @@
 import "./style.css";
 
 const $countOption = document.getElementById("count-number");
+const $saveBtn = document.getElementById("save-button");
+const $sorting = document.querySelectorAll("#sorting");
 
 $countOption.addEventListener("change", (event) => {
   const number = event.target.value;
 
   if (number < 5 || number > 25) {
-    $countOption.value = 5;
+    $countOption.value = 8;
   }
+});
+
+$saveBtn.addEventListener("click", () => {
+  const tabCount = $countOption.value;
+  let sorting = "";
+
+  $sorting.forEach((sortName) => {
+    if (sortName.checked) {
+      sorting = sortName.value;
+    }
+  });
+
+  chrome.storage.local.set({
+    tabCount,
+    sorting,
+  });
 });
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -3,9 +3,14 @@ import "./style.css";
 const $tabCount = document.getElementById("tab-count");
 const $onoff = document.getElementById("on-off-switch");
 
-const count = 8;
-$tabCount.textContent = count;
+chrome.storage.local.get(["tabCount"], (res) => {
+  $tabCount.textContent = "tabCount" in res ? res.tabCount : 8;
+});
 
 $onoff.addEventListener("click", () => {
   console.log($onoff.checked);
+  if (!$onoff.checked) {
+    // tab 제한 갯수를 무한으로 늘리거나 early return을 통해 작동 금지, 혹은 counting하는 것 비활성화 등드의 방법으로 할것
+    // 찾아본 결과 해당 extension을 강제로 비활성화 하기는 힘듬.
+  }
 });
